652fd2d6ef5f721454d810d0b96a117ba7159e2a
Eduardo San Martin Morote <posva13@gmail.com>
2016-12-07 17:02:38 +0100

    💄 Minor updates in index.js

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,4 +1,5 @@
 export default {
+  name: 'HelloJsx',
   data () {
     return {
       msg: 'Hello JSX'
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import Hello from './Hello.vue'
 import HelloJsx from './Hello.jsx'
 
-function unused () {
-  return 'Im never called'
-}
-unused
-
 export default Hello
 
 if (window.Vue) {
+  // here you should do Vue.use(YourPlugin)
   window.Vue.component('hello', Hello)
   window.Vue.component('hello-jsx', HelloJsx)
 }
